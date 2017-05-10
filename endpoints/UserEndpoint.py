7893616818b9c01489702ad8b26c80b0fab5379c
Peter Ullrich <peter.j.ullrich@gmail.com>
2017-05-10 17:23:47 +0200

    Added GET functionality to the User endpoint

--- endpoints/UserEndpoint.py
+++ endpoints/UserEndpoint.py
@@ -3,8 +3,18 @@ from endpoints.Endpoint import Endpoint
 
 class UserEndpoint(Endpoint):
 
-    endpoint = "user"
+    endpoint_user = "user"
+    endpoint_user_person = "user-person"
+    endpoint_user_company = "user-company"
 
-    @property
-    def user(self):
-        return self.api_client.get(self.endpoint).json()
+    def get_user(self):
+        return self.api_client.get(self.endpoint_user).json()["Response"]
+
+    def get_user_person_by_id(self, user_id):
+        endpoint = "%s/%d" % (self.endpoint_user_person, user_id)
+        return self.api_client.get(endpoint).json()["Response"]
+
+    def get_user_company_by_id(self, user_id):
+        endpoint = "%s/%d" % (self.endpoint_user_company, user_id)
+        return self.api_client.get(endpoint).json()["Response"]
+
