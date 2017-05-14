7b8cc93245fbc38503023280cf27c6bfe0f47aa9
Peter Ullrich <peter.j.ullrich@gmail.com>
2017-05-14 23:37:59 +0200

    Added an APIClient which can be used without a config file

---
+++ api_client_wo_config.py
@@ -0,0 +1,62 @@
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import serialization
+
+from api_client import ApiClient
+
+
+class ApiClientWOConfig(ApiClient):
+
+    def __init__(self, privkey_pem, token='', servkey_pem=''):
+        super().__init__()
+        self.__privkey_pem = privkey_pem
+        self.__token = token
+        self.__servkey_pem = servkey_pem
+
+    @property
+    def user_token(self):
+        return None
+
+    @property
+    def server_token(self):
+        return serialization.load_pem_public_key(
+            self.__servkey_pem,
+            backend=default_backend()
+        )
+
+    @property
+    def session_token(self):
+        return self.__token
+
+    @property
+    def pubkey(self):
+        return self.privkey_pem.public_key().public_bytes(
+            encoding=serialization.Encoding.PEM,
+            format=serialization.PublicFormat.SubjectPublicKeyInfo
+        )
+
+    @property
+    def privkey(self):
+        return serialization.load_pem_private_key(
+            self.__privkey_pem,
+            password=None,
+            backend=default_backend()
+        )
+
+    @property
+    def privkey_pem(self):
+        return self.__privkey_pem
+
+    @property
+    def api_key(self):
+        return None
+
+    @property
+    def server_pubkey(self):
+        return serialization.load_pem_public_key(
+            self.server_pubkey_pem,
+            backend=default_backend()
+        )
+
+    @property
+    def server_pubkey_pem(self):
+        return self.__servkey_pem
