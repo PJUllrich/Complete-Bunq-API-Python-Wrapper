374156fb270c039986e7ca4bdc2cfac90e3c68f3
Peter Ullrich <peter.j.ullrich@gmail.com>
2017-05-15 15:19:30 +0200

    Moved all files to a general package so that importing into other project is easier

---
+++ apiwrapper/tests/endpoints/test_endpoint.py
@@ -0,0 +1,24 @@
+from unittest.case import TestCase
+from unittest.mock import MagicMock
+
+from apiwrapper.clients.api_client import ApiClient
+
+
+class EndpointTest(TestCase):
+
+    def setUp(self, testing_class):
+        self.api_client = ApiClient()
+        self.test_class = testing_class(self.api_client)
+        self.test_class._make_get_request = MagicMock(
+            side_effect=self.side_effect)
+
+    def side_effect(self, endpoint):
+        return endpoint
+
+    def assert_endpoints(self, endpoint_should_be, endpoint_to_check):
+        assert (endpoint_to_check == endpoint_should_be), \
+            self.print_assertion_error(endpoint_should_be, endpoint_to_check)
+
+    def print_assertion_error(self, endpoint_should_be, endpoint_to_check):
+        return "Should be: " + endpoint_should_be + " but was: " + \
+          endpoint_to_check
