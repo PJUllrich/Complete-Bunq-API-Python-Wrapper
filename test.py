0948cca27af8a3fdba26ad1003f392c86b8c2c20
Peter Ullrich <peter.j.ullrich@gmail.com>
2017-05-16 12:15:06 +0200

    Finally fixed coverage to omit virtualenv/* in the test coverage stats.

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 def test():
     """Run the unit tests."""
     import unittest
-    tests = unittest.TestLoader().discover('apiwrapper/tests')
+    tests = unittest.TestLoader().discover('tests', top_level_dir='apiwrapper')
     unittest.TextTestRunner(verbosity=2).run(tests)
 
 test()
