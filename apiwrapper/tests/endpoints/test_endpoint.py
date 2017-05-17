4743d519390479c53e9851100c57379a3f0fb135
Peter Ullrich <peter.j.ullrich@gmail.com>
2017-05-17 13:49:31 +0200

    Finished writing unittests for the endpoints. Added the option to use an own config file.

--- apiwrapper/tests/endpoints/test_endpoint.py
+++ apiwrapper/tests/endpoints/test_endpoint.py
@@ -3,13 +3,32 @@ import uuid
 from unittest.case import TestCase
 from unittest.mock import MagicMock
 
-from apiwrapper.clients.api_client_persisting import ApiClientPersisting
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives.asymmetric import rsa
+
+from apiwrapper.clients.api_client import ApiClient
 
 
 class EndpointTest(TestCase):
+    @staticmethod
+    def create_random_privkey():
+        private_key = rsa.generate_private_key(
+            public_exponent=65537,
+            key_size=2048,
+            backend=default_backend()
+        )
+
+        return private_key.private_bytes(
+            encoding=serialization.Encoding.PEM,
+            format=serialization.PrivateFormat.PKCS8,
+            encryption_algorithm=serialization.NoEncryption()
+        ).decode()
+
+    random_privkey = create_random_privkey.__func__()
 
     def setUp(self, testing_class):
-        self.api_client = ApiClientPersisting(str(uuid.uuid4()))
+        self.api_client = ApiClient(self.random_privkey, str(uuid.uuid4()))
         self.test_class = testing_class(self.api_client)
         self.test_class._make_get_request = MagicMock(
             side_effect=self.side_effect_get)
@@ -28,5 +47,6 @@ class EndpointTest(TestCase):
         assert (check == should), \
             self.print_assertion_error(should, check)
 
-    def print_assertion_error(self, should, was):
+    @staticmethod
+    def print_assertion_error(should, was):
         return "Should be: %s but was: %s" % (should, was)
