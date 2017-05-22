f4884d38faa9a652dfc45a685dc50746efcbd981
Peter Ullrich <peter.j.ullrich@gmail.com>
2017-05-22 12:22:35 +0200

    AND deleting the test files from the apiwrapper folder

--- test.py
+++
@@ -1,7 +0,0 @@
-def test():
-    """Run the unit tests."""
-    import unittest
-    tests = unittest.TestLoader().discover('tests', top_level_dir='apiwrapper')
-    unittest.TextTestRunner(verbosity=2).run(tests)
-
-test()
